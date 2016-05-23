506a8286812acfc50c834b4cae9460e5b5a0297b
Ankhbayar Baasandorj <antony.baasan@gmail.com>
2016-05-22 23:48:15 -0600

    init upload

---
+++ app/app.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by Antony on 2016-05-22.
+ */
+function greeting(person:string) {
+    return "Hello, " + person;
+}
+
+document.body.innerHTML = greeting("Antony");
---
+++ app/comp/test.ts
@@ -0,0 +1,3 @@
+/**
+ * Created by Antony on 2016-05-22.
+ */
