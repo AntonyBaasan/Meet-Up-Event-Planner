a56036fee10baa956b6f7aacd136df9e8d0fac0c
Ankhbayar Baasandorj <antony.baasan@gmail.com>
2016-05-23 01:12:29 -0600

    add watchify

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,42 @@
 var gulp = require("gulp");
-// var ts = require("gulp-typescript");
-// var tsProject = ts.createProject("tsconfig.json");
 var clean = require('gulp-clean');
 var runSequence = require('run-sequence');
 var browserify = require("browserify");
 var source = require('vinyl-source-stream');
 var tsify = require("tsify");
+var watchify = require("watchify");
+var gutil = require("gulp-util");
 
 var output_dir = "dist";
+var output_js_file_name = "bundle.js";
 var paths = {
     pages: ['src/*.html']
 };
 
 gulp.task("copy-html", function () {
     return gulp.src(paths.pages)
-        .pipe(gulp.dest("dist"));
-});
-
-gulp.task("default", ["copy-html"], function () {
-    return browserify({
-        basedir: '.',
-        debug: true,
-        entries: ['src/app.ts'],
-        cache: {},
-        packageCache: {}
-    })
-        .plugin(tsify)
-        .bundle()
-        .pipe(source('bundle.js'))
         .pipe(gulp.dest(output_dir));
 });
+
+var watchedBrowserify = watchify(browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['src/app.ts'],
+    cache: {},
+    packageCache: {}
+})
+    .plugin(tsify));
+
+function bundle() {
+    return watchedBrowserify
+        .bundle()
+        .pipe(source(output_js_file_name))
+        .pipe(gulp.dest(output_dir));
+}
+
+gulp.task("default", ["copy-html"], bundle);
+watchedBrowserify.on("update", bundle);
+watchedBrowserify.on("log", gutil.log);
 
 // gulp.task("default", function (cb) {
 //     runSequence(
--- src/comp/greet.ts
+++ src/comp/greet.ts
@@ -2,5 +2,5 @@
  * Created by Antony on 2016-05-22.
  */
 export function sayHello(name: string) {
-    return `Hello from ${name}`;
+    return `Hello my name is ${name}`;
 }
